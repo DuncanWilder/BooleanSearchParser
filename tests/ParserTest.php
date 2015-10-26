faa3fe9382ccc744bacbf42c06ae5225a315f191
Duncan Ogle <duncan.ogle@gmail.com>
2015-10-26 15:59:25 +0000

    Added phpunit and a simple test

---
+++ tests/ParserTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duncan
+ * Date: 26/10/15
+ * Time: 15:58
+ */
+
+namespace BooleanSearchParser;
+
+
+class ParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+        $this->assertEquals(0, 0);
+    }
+}
